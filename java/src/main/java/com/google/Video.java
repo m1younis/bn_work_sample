e7f0749fcca19cf2e93e9dad1143853a7c5f6611
Moeen <moeen.younis@city.ac.uk>
2021-08-25 18:51:03 +0100

    `Video` - additions made to implement flag/unflag

--- java/src/main/java/com/google/Video.java
+++ java/src/main/java/com/google/Video.java
@@ -10,18 +10,21 @@ public class Video {
      private final String id;
      private final List<String> tags;
 
+     private boolean flagged = false;                            // Added as part of FLAG_VIDEO
+     private String reason = null;                               // Added as part of FLAG_VIDEO
+
      public Video(String title, String id, List<String> tags) {
           this.title = title;
           this.id = id;
           this.tags = Collections.unmodifiableList(tags);
      }
 
-     /** Returns the title of the video. */
+     /** Returns the video's title. */
      public String getTitle() {
           return this.title;
      }
 
-     /** Returns the video id of the video. */
+     /** Returns the video's URL. */
      public String getVideoId() {
           return this.id;
      }
@@ -29,5 +32,24 @@ public class Video {
      /** Returns a read-only collection of a video's tags. */
      public List<String> getTags() {
           return this.tags;
+     }
+
+     /** Returns the video's "flagged" status. */
+     public boolean isFlagged() {
+          return this.flagged;
+     }
+
+     /** Returns the reason for which the video has been flagged. */
+     public String getFlag() {
+          return this.reason;
+     }
+
+     /** Marks the video as "flagged" with a given reason, if supplied. */
+     public void flag(String reason) {
+          this.flagged = true;
+          if (reason == null)
+               this.reason = "Not supplied";
+          else
+               this.reason = reason;
      }
 }
